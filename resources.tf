5458022ffef87e2b80a6c26e8c136265cb5403d0
mitsuko0325 <arishou1234@gmail.com>
2021-09-24 07:56:59 +0000

    first

---
+++ resources.tf
@@ -0,0 +1,75 @@
+variable "datacenter" {}
+
+
+
+data "vsphere_datacenter" "dc" {
+  name = "${var.datacenter}"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "ds1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "manta-tokyo-hx-cluster"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "terraform_dev_sarimits|main_ap|main_epg"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "ubuntu-cloudimg-temp"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+
+  # https://github.com/hashicorp/terraform-provider-vsphere/issues/489
+  cdrom {
+    client_device = true
+  }
+
+  name             = "terraform-test"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  # datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  # num_cpus = 2
+  # memory   = 1024
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+
+    customize {
+      linux_options {
+        host_name = "terraform-test"
+        domain    = "test.internal"
+      }
+
+      network_interface {
+        ipv4_address = "10.0.0.10"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "10.0.0.1"
+    }
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+vsphere_user = "administrator@manta.tokyo"
+vsphere_password = "C!scoI23"
+vsphere_server = "172.20.12.1"
+datacenter = "manta-tokyo"
